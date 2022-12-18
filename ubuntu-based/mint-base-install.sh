0b4e4a50be36003b707aca157aa882cb5671f91c
guyintheroom <guyintheroom@gmail.com>
2022-12-18 01:21:55 -0500

    12.18.22

---
+++ ubuntu-based/mint-base-install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+echo Base Install - intended for Linux Mint!
+read
+
+echo :::::::
+echo Initial Things
+cd
+sudo apt update
+sudo apt upgrade -y
+
+echo :::::::
+echo Installing Nala
+echo "deb https://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
+wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
+sudo apt update
+sudo apt install nala -y
+echo pausing script - if  this didn't work, restart this script
+read
+
+echo :::::::
+echo Importing Terminal Profile
+sudo nala install dconf-editor -y
+dconf load /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ < ~/all-the-things/config-files/terminal-profile.dconf
+
+echo :::::::
+echo Installing Chrome
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+echo :::::::
+echo Removing Firefox
+sudo nala purge firefox -y
+sudo snap remove firefox
+echo pausing script
+read
+
+echo :::::::
+echo Installing Random Apps
+sudo nala install neofetch pavucontrol mpv bashtop tldr trash-cli cmatrix -y
+
+echo :::::::
+echo Job Done!! Press any key to close!
+read
