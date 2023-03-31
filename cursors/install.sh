bcf33070a7baf89b61ee37b094f9ed1eace69fe5
guyintheroom <guyintheroom@gmail.com>
2023-03-31 10:00:12 -0400

    03.31.23

--- cursors/install.sh
+++ cursors/install.sh
@@ -5,7 +5,7 @@ echo :::::::
 echo Installing Bibata Modern Ice
 cd ~/all-the-things/cursors/
 tar -xvf Bibata-Modern-Ice.tar.gz
-sudo mv Bibata-Modern-Ice/ ~/.icons/
+sudo mv Bibata-Modern-Ice ~/.icons/
 
 echo :::::::
 echo Job Done!!
