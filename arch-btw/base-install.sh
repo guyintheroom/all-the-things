10288c8a91a207783d6673b94f2ee3c615d29996
guyintheroom <guyintheroom@gmail.com>
2023-04-21 12:13:01 -0400

    04.21.23

--- arch-btw/base-install.sh
+++ arch-btw/base-install.sh
@@ -30,21 +30,14 @@ yay -S pop-fonts --noconfirm
 yay -S ttf-roboto-slabs --noconfirm
 yay -S ttf-ubuntu-font-family --noconfirm
 yay -S ttf-meslo-nerd-font-powerlevel10k --noconfirm
+read
 
 # Installing Icon Themes
 yay -S yaru-icon-theme --noconfirm
 yay -S bibata-cursor-theme --noconfirm
 yay -S se98-icon-theme-git --noconfirm
 yay -S elementary-icon-theme --noconfirm
-
-# Terminal Things
-yay -S --needed gnome-terminal-transparency --noconfirm
-sudo pacman -S --needed dconf-editor --noconfirm
-dconf load /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ < ~/all-the-things/config-files/terminal-profile.dconf
-sudo pacman -S zsh
-yay -S --noconfirm zsh-theme-powerlevel10k-git
-echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-chsh -s /usr/bin/zsh
+read
 
 # Google Drive and Chrome
 sudo pacman -S --needed gnome-online-accounts gvfs-goa gvfs-google --noconfirm
@@ -60,6 +53,18 @@ sudo pacman -S --needed libgtop lm_sensors gnome-icon-theme-symbolic gnome-icon-
 sudo pacman -S --needed gnome-tweaks --noconfirm
 yay -S --needed gnome-browser-connector --noconfirm
 
+# Terminal Things
+yay -S --needed gnome-terminal-transparency --noconfirm
+sudo pacman -S --needed dconf-editor --noconfirm
+dconf load /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ < ~/all-the-things/config-files/gnome-terminal/arch.dconf
+sudo pacman -S zsh
+yay -S --noconfirm zsh-theme-powerlevel10k-git
+echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+cp -rT ~/all-the-things/config-files/dot-files ~/
+read
+chsh -s /usr/bin/zsh
+
+echo :::::::
 echo Job Done!!
 echo Reboot Needed!!
 echo :::::::
