e49abcece371cc1899a55743d01e2d44ef2b63e6
guyintheroom <guyintheroom@gmail.com>
2023-08-25 18:17:44 -0400

    08.25.23

--- arch-btw/themes.sh
+++ arch-btw/themes.sh
@@ -10,7 +10,6 @@ echo # Installing Fonts
 yay -S pop-fonts --noconfirm
 yay -S ttf-roboto-slabs --noconfirm
 yay -S ttf-ubuntu-font-family --noconfirm
-sudo pacman -S --needed ttf-dejavu ttf-opensans ttf-croscore ttf-roboto ttf-linux-libertine ttf-wqy-zenhei --noconfirm
 
 echo # Installing Icon Themes
 yay -S yaru-icon-theme --noconfirm
