2ddb33b27eca0c405a9251f58123325946de8b5f
guyintheroom <guyintheroom@gmail.com>
2023-08-25 17:36:15 -0400

    08.25.23

--- arch-btw/desktop-things.sh
+++ arch-btw/desktop-things.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-echo # Extra Apps for Main Desktop!!
+# Extra Things for Main Desktop!
 
-echo # Installing Multimedia Things
+# Installing Multimedia Things
 sudo pacman -S --needed audacious spotify-launcher yt-dlp --noconfirm
 yay -S --needed plex-media-player --noconfirm
 
-echo # Installing Editing Apps
+# Installing Editing Apps
 sudo pacman -S --needed pinta krita kdenlive ardour obs-studio flowblade --noconfirm
 
-echo # Installing Misc apps
+# Installing Misc apps
 sudo pacman -S --needed bitwarden --noconfirm
 yay -S --needed drawio-desktop-bin --noconfirm
+
+# Installing Font-Repo
+mkdir ~/.local/share/fonts
+cp -rvT ~/all-the-things/fonts ~/.local/share/fonts/
 
 echo # Bluetooth Things
 sudo pacman -S --needed bluez bluez-utils --noconfirm
@@ -18,5 +22,5 @@ sudo systemctl start bluetooth.service
 sudo systemctl enable bluetooth.service
 
 echo :::::::
-echo Job Done!!
+echo Job Done!
 echo :::::::
