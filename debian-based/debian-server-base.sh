db92234d6c677f2c2a248820f74025ed34a9ce46
guyintheroom <guyintheroom@gmail.com>
2023-09-16 00:21:58 -0400

    09.15.23

--- debian-based/debian-server-base.sh
+++ debian-based/debian-server-base.sh
@@ -9,6 +9,7 @@ sudo apt upgrade -y
 # Installing Basic Apps
 sudo apt install nala -y
 sudo nala install neofetch timeshift curl docker.io -y
+cp -rT ~/all-the-things/config-files/dot-files/.bash_aliases ~/.bash_aliases
 
 echo :::::::
 echo Job Done!
