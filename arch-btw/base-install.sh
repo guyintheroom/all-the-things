964aa9d8ce9479164629217dad5ff317e10c4d0c
guyintheroom <guyintheroom@gmail.com>
2023-09-30 09:49:48 -0400

    09.30.23

--- arch-btw/base-install.sh
+++ arch-btw/base-install.sh
@@ -18,13 +18,13 @@ makepkg -si --noconfirm
 cd
 
 # Installing Basic Apps
-sudo pacman -S --needed neofetch discord pavucontrol inkscape vlc yt-dlp archlinux-wallpaper unrar arch-audit speedtest-cli thefuck --noconfirm
+sudo pacman -S --needed neofetch discord pavucontrol inkscape vlc yt-dlp archlinux-wallpaper unrar arch-audit --noconfirm
 yay -S --needed google-chrome rpi-imager timeshift mission-center mkinitcpio-firmware downgrade rate-mirrors-bin --noconfirm
 cp -rT ~/all-the-things/yt-dl/ ~/Downloads/yt-dl/
 
 # Terminal Witchcraft!
 # Pre-Reqs and Emulator
-sudo pacman -S --needed alacritty zsh zsh-completions tldr --noconfirm
+sudo pacman -S --needed alacritty zsh zsh-completions tldr speedtest-cli thefuck bat --noconfirm
 yay -S alacritty-themes ttf-meslo-nerd-font-powerlevel10k --noconfirm
 mkdir ~/.config/alacritty
 cp -rT ~/all-the-things/config-files/dot-files/alacritty.yml ~/.config/alacritty/alacritty.yml
