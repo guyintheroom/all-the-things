c26ed5706e16918c47af36055a7d1c6c0b0ecb9c
guyintheroom <guyintheroom@gmail.com>
2024-02-16 12:36:53 -0500

    02.16.24

--- arch-btw/base-install.sh
+++ arch-btw/base-install.sh
@@ -46,6 +46,7 @@ yay -S ttf-meslo-nerd-font-powerlevel10k --noconfirm
 mkdir ~/.config/kitty
 cp -rT ~/all-the-things/config-files/dot-files/kitty/kitty.conf ~/.config/kitty/kitty.conf
 cp -rT ~/all-the-things/config-files/dot-files/kitty/current-theme.conf ~/.config/kitty/current-theme.conf
+cp -rT ~/all-the-things/images/icons/kitty-tiny.png ~/.config/kitty/kitty-tiny.png
 sed -e 's|Icon=.*|Icon=/home/guyintheroom/all-the-things/images/icons/kitty.app.png|' /usr/share/applications/kitty.desktop > ~/.local/share/applications/kitty.desktop
 
 # Plugins
