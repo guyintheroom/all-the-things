91219d34ab7fee52d833e8afc2d7b7287289789d
guyintheroom <guyintheroom@gmail.com>
2024-02-16 14:13:02 -0500

    02.16.24

--- arch-btw/kitty-migration.sh
+++ arch-btw/kitty-migration.sh
@@ -6,6 +6,7 @@ sudo pacman -S --needed kitty --noconfirm
 mkdir ~/.config/kitty
 cp -rT ~/all-the-things/config-files/dot-files/kitty/kitty.conf ~/.config/kitty/kitty.conf
 cp -rT ~/all-the-things/config-files/dot-files/kitty/current-theme.conf ~/.config/kitty/current-theme.conf
+cp -rT ~/all-the-things/images/icons/kitty-tiny.png ~/.config/kitty/kitty-tiny.png
 sed -e 's|Icon=.*|Icon=/home/guyintheroom/all-the-things/images/icons/kitty.app.png|' /usr/share/applications/kitty.desktop > ~/.local/share/applications/kitty.desktop
 
 echo :::::::
